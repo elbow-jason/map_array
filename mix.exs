5131edf72b344dcf41faaeb5b0d95f65e106432b
Jason Goldberger <jlgoldb2@asu.edu>
2019-08-16 21:55:54 -0700

    prepped for publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule MapArray.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,8 +23,24 @@ defmodule MapArray.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.20.2", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "A utility for using ordered maps as if they were arrays"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "map_array",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE CHANGELOG.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/elbow-jason/map_array"}
     ]
   end
 end
